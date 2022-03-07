e4fcc3210996370444a6a2046199ca5f024ca0c5
Janamajaya Mall <janmajaya.choithram@gmail.com>
2022-03-08 04:38:48 +0800

    removed ssl auth

--- models/index.js
+++ models/index.js
@@ -4,16 +4,16 @@ const UrlCache = require("./urlCache");
 
 const connectDb = () => {
 	let options = {};
-	if (process.env.NODE_ENV === "production") {
-		options = {
-			ssl: true,
-			sslValidate: true,
-			sslCA: `${__dirname}/rds-combined-ca-bundle.pem`,
-			replicaSet: "rs0",
-			readPreference: "secondaryPreferred",
-			retryWrites: false,
-		};
-	}
+	// if (process.env.NODE_ENV === "production") {
+	// 	options = {
+	// 		ssl: true,
+	// 		sslValidate: true,
+	// 		sslCA: `${__dirname}/rds-combined-ca-bundle.pem`,
+	// 		replicaSet: "rs0",
+	// 		readPreference: "secondaryPreferred",
+	// 		retryWrites: false,
+	// 	};
+	// }
 	return mongoose.connect(process.env.DB_URI, options);
 };
 
